94fa428ff9f81aa85c95ca39048fe64a8196c937
Cosmin Stoian <cosmin.stn1997@gmail.com>
2021-01-06 23:54:54 +0200

    Completed lessons 3 and 4.

--- lesson3-convolutions-and-pooling.py
+++ lesson3-convolutions-and-pooling.py
@@ -54,3 +54,25 @@ plt.grid(False)
 plt.imshow(i_transformed)
 # plt.axis('off')
 plt.show()
+
+# Pooling
+new_x = int(size_x/2)
+new_y = int(size_y/2)
+newImage = np.zeros((new_x, new_y))
+for x in range(0, size_x, 2):
+    for y in range(0, size_y, 2):
+        # Load the 4 pixels into the array
+        pixels = []
+        pixels.append(i_transformed[x, y])
+        pixels.append(i_transformed[x+1, y])
+        pixels.append(i_transformed[x, y+1])
+        pixels.append(i_transformed[x+1, y+1])
+        pixels.sort(reverse=True)
+        # Use just the 1 pixel with the maximum value
+        newImage[int(x/2), int(y/2)] = pixels[0]
+
+# The new image will be 1/4 of the original size
+plt.gray()
+plt.grid(False)
+plt.imshow(newImage)
+plt.show()
